a9af2e08c589048ee32aea4aa9332cf7121d3da4
Balpreet-Dhillon <109980525+Balpreet-Dhillon@users.noreply.github.com>
2023-01-23 10:14:19 -0800

    first commit

---
+++ app.js
@@ -0,0 +1,42 @@
+const form = document.querySelector(".form");
+const userInput = document.querySelector(".user-input");
+const checkBtn = document.querySelector("#check-btn");
+const answerOutput = document.querySelector(".vowel-count");
+let vowels = ["a", "e", "i", "o", "u", "A", "E", "I", "O", "U"];
+let vowelCount = 0;
+
+function getCount(e) {
+
+    e.preventDefault();
+
+    if(userInput.value === ""){
+
+        const errorMsg = document.createElement("div");
+        errorMsg.classList.add("error-msg");
+        errorMsg.innerText = "Please Fill In Field";
+        answerOutput.appendChild(errorMsg);
+
+    }else{
+
+    let word = document.getElementById("user-input").value
+    for(w of word){
+        for(v of vowels){
+
+            if(w == v){
+
+                vowelCount++
+
+            }
+        }
+    }
+
+    answerOutput.innerText = `${word} has ${vowelCount} vowels`
+
+    userInput.value = "";
+    vowelCount = 0;
+
+    }
+}
+
+checkBtn.addEventListener("click", getCount);
+
